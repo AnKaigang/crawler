87f095b1e2b9626874a28fba7609e2863bc9e6d2
ankaigang@huoqiu.cn <ankaigang@huoqiu.cn>
2017-07-20 10:10:43 +0800

    add

--- src/main/java/cn/akgang/util/HttpUtil.java
+++ src/main/java/cn/akgang/util/HttpUtil.java
@@ -5,26 +5,49 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 import java.util.Map.Entry;
 
 /**
  * Created by akgang on 2017/5/25.
+ * <p>
+ * 发送http(s)请求的工具类包
  */
 public class HttpUtil {
 
 
-    public String sendHttpPost() {
-
-        return "";
+    /**
+     * 发送http的post请求
+     *
+     * @param url
+     * @param header
+     * @param paramMap
+     * @param jsonBody
+     * @param charset
+     * @return
+     * @throws IOException
+     */
+    public static String sendHttpPost(String url, Map<String, String> header, Map<String, String> paramMap, String jsonBody, String charset) throws IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = generatorPostRequest(url, header, paramMap, jsonBody, charset);
+        String result = null;
+        HttpResponse response = httpClient.execute(httpPost);
+        if (response != null) {
+            HttpEntity resEntity = response.getEntity();
+            if (resEntity != null) {
+                result = EntityUtils.toString(resEntity, charset);
+            }
+        }
+        return result;
     }
 
     /**
@@ -36,51 +59,120 @@ public class HttpUtil {
      * @param charset
      * @return
      */
-    public String sendHttpsPost(String url, Map<String, String> paramMap, String jsonBody, String charset) {
-        HttpClient httpClient = null;
-        HttpPost httpPost = null;
+    public static String sendHttpsPost(String url, Map<String, String> header, Map<String, String> paramMap, String jsonBody, String charset) throws Exception {
+        HttpClient httpClient = new SSLClient();
+        HttpPost httpPost = generatorPostRequest(url, header, paramMap, jsonBody, charset);
         String result = null;
-        try {
-            httpClient = new SSLClient();
-            httpPost = new HttpPost(url);
-            httpPost.setHeader("Content type", "application/json");
-            //设置参数
-            List<NameValuePair> list = new ArrayList<NameValuePair>();
-            if (paramMap != null) {
-                Iterator iterator = paramMap.entrySet().iterator();
-                while (iterator.hasNext()) {
-                    Entry<String, String> elem = (Entry<String, String>) iterator.next();
-                    list.add(new BasicNameValuePair(elem.getKey(), elem.getValue()));
-                }
+        HttpResponse response = httpClient.execute(httpPost);
+        if (response != null) {
+            HttpEntity resEntity = response.getEntity();
+            if (resEntity != null) {
+                result = EntityUtils.toString(resEntity, charset);
             }
-            if (list.size() > 0) {
-                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list, charset);
-                httpPost.setEntity(entity);
-            }
-            if (!StringUtils.isEmpty(jsonBody)) {
-                StringEntity stringEntity = new StringEntity(jsonBody, charset);
-                httpPost.setEntity(stringEntity);
-            }
-            HttpResponse response = httpClient.execute(httpPost);
-            if (response != null) {
-                HttpEntity resEntity = response.getEntity();
-                if (resEntity != null) {
-                    result = EntityUtils.toString(resEntity, charset);
-                }
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
         }
         return result;
     }
 
-    public String sendHttpGet(String url) {
-        HttpPost httpPost=new HttpPost(url);
-
-        return "";
+    /**
+     * 发送http的get请求
+     *
+     * @param url
+     * @param header
+     * @param charset
+     * @return
+     * @throws IOException
+     */
+    public static String sendHttpGet(String url, Map<String, String> header, String charset) throws IOException {
+        HttpGet httpGet = generatorGetRequest(url, header);
+        HttpClient httpClient = new DefaultHttpClient();
+        String result = null;
+        HttpResponse response = httpClient.execute(httpGet);
+        if (response != null) {
+            HttpEntity resEntity = response.getEntity();
+            if (resEntity != null) {
+                result = EntityUtils.toString(resEntity, charset);
+            }
+        }
+        return result;
     }
 
-    public String sendHttpsGet() {
-        return "";
+    /**
+     * 发送https的get请求
+     *
+     * @param url
+     * @param header
+     * @param charset
+     * @return
+     * @throws Exception
+     */
+    public static String sendHttpsGet(String url, Map<String, String> header, String charset) throws Exception {
+        HttpClient httpClient = new SSLClient();
+        HttpGet httpGet = generatorGetRequest(url, header);
+        String result = null;
+        HttpResponse response = httpClient.execute(httpGet);
+        if (response != null) {
+            HttpEntity resEntity = response.getEntity();
+            if (resEntity != null) {
+                result = EntityUtils.toString(resEntity, charset);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 生成post请求格式
+     *
+     * @param url
+     * @param header
+     * @param paramMap
+     * @param jsonBody
+     * @param charset
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    public static HttpPost generatorPostRequest(String url, Map<String, String> header, Map<String, String> paramMap, String jsonBody, String charset) throws UnsupportedEncodingException {
+        HttpPost httpPost = new HttpPost(url);
+        //设置请求头
+        if (header != null) {
+            for (String key : header.keySet()) {
+                httpPost.setHeader(key, header.get(key));
+            }
+        }
+        //设置参数
+        List<NameValuePair> list = new ArrayList<NameValuePair>();
+        if (paramMap != null) {
+            for (String key : paramMap.keySet()) {
+                list.add(new BasicNameValuePair(key, paramMap.get(key)));
+            }
+        }
+        if (list.size() > 0) {
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list, charset);
+            httpPost.setEntity(entity);
+        }
+        //设置请求体
+        if (!StringUtils.isEmpty(jsonBody)) {
+            StringEntity stringEntity = new StringEntity(jsonBody, charset);
+            httpPost.setEntity(stringEntity);
+        }
+        return httpPost;
+    }
+
+    /**
+     * 生成get请求格式
+     *
+     * @param url
+     * @param header
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    public static HttpGet generatorGetRequest(String url, Map<String, String> header) throws UnsupportedEncodingException {
+        HttpGet httpGet = new HttpGet(url);
+        //设置请求头
+        if (header != null) {
+            for (String key : header.keySet()) {
+                httpGet.setHeader(key, header.get(key));
+            }
+        }
+        return httpGet;
     }
 }
